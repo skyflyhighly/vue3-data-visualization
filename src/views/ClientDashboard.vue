db7f592374efa9be452d988d65b344de226950a7
skyflyhighly <skyflyhighly@gmail.com>
2023-05-27 00:22:46 +0800

    Init project

---
+++ src/views/ClientDashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dashboard">
+    <!-- your components -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+  name: 'ClientDashboard',
+  setup() {
+    // your code
+  }
+})
+</script>
+<style lang="scss">
+
+</style>
